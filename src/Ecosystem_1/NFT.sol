7f021e3d46db607771533aa913fcb371c0cf2c03
Yves Boutellier <63062257+yvesbou@users.noreply.github.com>
2024-11-09 18:31:08 +0700

    nft created

--- src/Ecosystem_1/NFT.sol
+++ src/Ecosystem_1/NFT.sol
@@ -3,27 +3,52 @@
 pragma solidity 0.8.28;
 
 import {ERC721Royalty} from "@openzeppelin-contracts-5.1.0/token/ERC721/extensions/ERC721Royalty.sol";
-
-// un-used
-import {ERC721} from "@openzeppelin-contracts-5.1.0/token/ERC721/ERC721.sol";
-import {IERC721} from "@openzeppelin-contracts-5.1.0/token/ERC721/IERC721.sol";
-import {IERC721Metadata} from "@openzeppelin-contracts-5.1.0/interfaces/IERC721Metadata.sol";
-import {ERC2981} from "@openzeppelin-contracts-5.1.0/token/common/ERC2981.sol";
-import {IERC2981} from "@openzeppelin-contracts-5.1.0/interfaces/IERC2981.sol";
+import {BitMaps} from "@openzeppelin-contracts-5.1.0/utils/structs/BitMaps.sol";
+import {MerkleProof} from "@openzeppelin-contracts-5.1.0/utils/cryptography/MerkleProof.sol";
 
 contract NFT is ERC721Royalty {
-    uint256 private _tokenIdCounter;
+    uint256 private _tokenIdCounter = 8; // the first 8 ids (0,...7) are for merkletree
+    bytes32 public merkleRoot;
+    BitMaps.BitMap private _claimStatus;
 
+    error AlreadyClaimed();
+    error NotEnoughPaid();
     error MaxSupplyReached();
 
-    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+    constructor(string memory name_, string memory symbol_, bytes32 merkleRoot_) ERC721(name_, symbol_) {
         _setDefaultRoyalty(msg.sender, 250);
+        merkleRoot = merkleRoot_;
     }
 
-    // TODO: add merkle tree
-    function mint() external {
+    /**
+     * - bitmap if claims claimed
+     * - merkle tree stored to have claims
+     */
+    function mint() external payable {
+        if (msg.value < 2 ether) revert NotEnoughPaid();
         _tokenIdCounter++;
         if (_tokenIdCounter == 1000) revert MaxSupplyReached();
         _safeMint(msg.sender, _tokenIdCounter);
+    }
+
+    function mintWithDiscount(bytes32[] calldata proof, uint256 index, uint256 amount) external payable {
+        // check if already claimed
+        if (BitMaps.get(_claimStatus, index)) revert AlreadyClaimed();
+
+        // verify proof address, index, amount
+        _verifyProof(proof, msg.sender, index, amount);
+
+        if (msg.value < 1 ether) revert NotEnoughPaid(); // 50% discount
+
+        // set claimed
+        BitMaps.setTo(_claimStatus, index, true);
+
+        // mint
+        _safeMint(msg.sender, amount); // where is the discount though?
+    }
+
+    function _verifyProof(bytes32[] calldata proof, address claimer, uint256 index, uint256 amount) private view {
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(claimer, index, amount))));
+        require(MerkleProof.verify(proof, merkleRoot, leaf));
     }
 }
--- src/Ecosystem_1/Staking.sol
+++ src/Ecosystem_1/Staking.sol
@@ -2,4 +2,6 @@
 
 pragma solidity 0.8.28;
 
+// receives NFT (possess it, only the same user can withdraw it)
+// NFT stakers can withdraw 10 ERC20 tokens every 24h
 contract Staking {}
